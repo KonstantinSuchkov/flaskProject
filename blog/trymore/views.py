3d8e49a9826f894ddbefe6b5a9c5864ab5b72112
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-03-28 17:46:26 +0300

    updated pyglet

--- blog/trymore/views.py
+++ blog/trymore/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from datetime import datetime
 import pytz
 
+from blog.configs import PATH_MP3
 
 trymore_app = Blueprint('trymore_app', __name__, url_prefix='/', static_folder='../static')
 
@@ -17,14 +18,10 @@ def text_to_mp3(text='', language='ru'):
 @trymore_app.route("/audio", endpoint="audio")
 def audio():
     import time
-    import pygame
-    from blog.configs import PATH_MP3
-    pygame.mixer.init()
-    pygame.mixer.music.load(f'{PATH_MP3}/text1.mp3')
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():  # wait for music to finish playing
-        time.sleep(1)
-    pygame.mixer.quit()
+    import pyglet
+    song = pyglet.media.load(f'{PATH_MP3}/text1.mp3')
+    song.play()
+    time.sleep(5)
     result = 'done'
     return render_template('trymore/list.html', result=result)
 
